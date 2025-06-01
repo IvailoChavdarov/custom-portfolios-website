1928c3b2a45901e6dd208478fa334a3b436c3b1d
Ivailo Chavdarov <ivailochav@gmail.com>
2025-06-01 19:06:23 +0300

    Added first and last name for the accounts

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,11 +2,15 @@ import { useAuth } from '../../contexts/AuthContext';
 import React, {useRef, useState} from 'react';
 import {Form, Button, Card} from 'react-bootstrap'
 import { Container } from 'react-bootstrap';
+import {db} from "../../firebase.js";
+import { doc, setDoc } from 'firebase/firestore';
 
 export default function Signup(){
     const emailRef = useRef()
     const passwordRef = useRef()
     const confirmPasswordRef = useRef()
+    const firstNameRef = useRef()
+    const lastNameRef = useRef()
     const [error, setError] = useState('')
 
     const {signup} = useAuth()
@@ -14,12 +18,19 @@ export default function Signup(){
     async function handleSubmit(e){
         e.preventDefault();
         //TODO: add validation (password match, etc.), loading screen, disable buttons
+
         try{
-            await signup(emailRef.current.value, passwordRef.current.value)
+            await signup(emailRef.current.value, passwordRef.current.value).then(credentials =>{
+                    setDoc(doc(db, 'profiles', credentials.user.uid), {
+                        firstName: firstNameRef.current.value,
+                        lastNameRef: lastNameRef.current.value
+                    })
+                }
+            )
+            
         }
         catch(er){
             setError('Account creation not successful')
-            console.log(er)
         }
         
     }
@@ -30,6 +41,14 @@ export default function Signup(){
             <Card className='w-100 p-5 mt-5'>
                 <h2 className='text-center mb-3'>Sign up</h2>
                 <Form onSubmit={handleSubmit}>
+                    <Form.Group id="firstName">
+                        <Form.Label>First name</Form.Label>
+                        <Form.Control type="text" ref={firstNameRef} required/>
+                    </Form.Group>
+                    <Form.Group id="lastName">
+                        <Form.Label>Last name</Form.Label>
+                        <Form.Control type="text" ref={lastNameRef} required/>
+                    </Form.Group>
                     <Form.Group id="email">
                         <Form.Label>email</Form.Label>
                         <Form.Control type="email" ref={emailRef} required/>
